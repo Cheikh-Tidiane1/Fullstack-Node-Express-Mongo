51e77e6e94be2617cee0b130fc8703e6a7f72c10
cheikh tidiane <cheikh951812@gmail.com>
2023-11-08 20:42:35 +0100

    ajout des router

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const bodyParser =  require('body-parser')
 const mongoose = require('mongoose');
-const Thing = require('./models/thing')
-
+const stuffRoutes = require('./routes/stuff')
 
 mongoose.connect('mongodb+srv://Gghost_:warnqblYXy4IYSmT@cluster0.smlwmdq.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -13,8 +12,8 @@ mongoose.connect('mongodb+srv://Gghost_:warnqblYXy4IYSmT@cluster0.smlwmdq.mongod
 
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -25,40 +24,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json())
 
-app.post('/api/stuff', (req, res, next) => {
-  delete req.body._id;
-  const thing = new Thing({
-    ...req.body
-  });
-  thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => {
-      console.log(error);
-      res.status(400).json({ error })});
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/stuff', (req, res, next) => {
-  Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({ error }));
-});
+app.use('/api/stuff',stuffRoutes)
 
 module.exports = app;
