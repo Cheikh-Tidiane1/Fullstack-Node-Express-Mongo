5403dab156954b3e4a491f8ef44b98d273f957c1
cheikh tidiane <cheikh951812@gmail.com>
2023-11-10 01:10:31 +0100

    Fn auth

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const router = express.Router();
 const stuffCtrl = require('../controllers/stuff')
+const auth = require('../controllers/auth')
+const router = express.Router();
 
-router.post("/", stuffCtrl.createThing);
-router.put("/:id", stuffCtrl.modifyThing);
-router.delete("/:id", stuffCtrl.deleteThing);
-router.get("/:id", stuffCtrl.getOneThing);
-router.get("/" ,stuffCtrl.getAllThing);
+router.get("/",auth,stuffCtrl.getAllThing);
+router.get("/:id",auth,stuffCtrl.getOneThing);
+router.post("/",auth,stuffCtrl.createThing);
+router.put("/:id",auth,stuffCtrl.modifyThing);
+router.delete("/:id",auth,stuffCtrl.deleteThing);
+
 
 module.exports = router;
